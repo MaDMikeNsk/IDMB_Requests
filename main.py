be57615b814c6fbf31aff88579592e3a5491015b
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-11-18 16:45:48 +0700

    #4 Added 'Emilia Clarke'

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ session.is_actor_in_movie('Jesse Eisenberg', 'The Social Network')
 
 session.is_actor_in_movie('Jim Carrey', 'The Truman Show')
 
+session.is_actor_in_movie('Emilia Clarke', 'Game of Thrones')
 
 
 # 'Jesse Eisenberg'
